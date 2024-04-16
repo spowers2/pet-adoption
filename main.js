f49c0e8e968d5ef6faf48d32bb5c78a0c08065a0
Scott Powers <spowers2@me.com>
2024-04-16 07:38:33 -0400

    added weather data to main.js

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-const weatherData = fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
-console.log(weatherData)
+async function getWData() {
+  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
+  const weatherData = await weatherPromise.json()
+  console.log(weatherData)
+}
+start()
