3458353e37cf7da1bcef7bbeeefce96754c89fb7
Scott Powers <spowers2@me.com>
2024-04-17 08:23:28 -0400

    Nearly finished pet card feature

--- main.js
+++ main.js
@@ -13,14 +13,27 @@ getWData()
 async function getPetData() {
   const petPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
   const petData = await petPromise.json()
-  petData.forEach(element => {
+  petData.forEach(pet => {
     const clone = template.content.cloneNode(true)
-    clone.querySelector("h3").textContent = element.name
-    clone.querySelector(".pet-description").textContent = element.species
-    clone.querySelector("img").src = element.photo
-    clone.querySelector(".pet-age").textContent = element.age
+    clone.querySelector("h3").textContent = pet.name
+    clone.querySelector(".pet-description").textContent = pet.description
+    clone.querySelector(".pet-card-photo img").src = pet.photo
+    clone.querySelector(".pet-card-photo img").alt = `Photo of ${pet.name} the ${pet.species}`
+    clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear)
     wrapper.appendChild(clone)
   })
   document.querySelector(".list-of-pets").appendChild(wrapper)
 }
+
 getPetData()
+
+function createAgeText(birthYear) {
+  const age = new Date().getFullYear() - birthYear
+  if (age === 1) {
+    return "1 year old"
+  } else if (age === 0) {
+    return "Your new baby! Less than 1 year old"
+  } else {
+    return `${age} years old`
+  }
+}
