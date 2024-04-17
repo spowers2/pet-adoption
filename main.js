73b048cc79b3ae583565e7bbd7bb0e8b1be5e52e
Scott Powers <spowers2@me.com>
2024-04-17 07:40:00 -0400

    Connected to pet data

--- main.js
+++ main.js
@@ -6,3 +6,14 @@ async function getWData() {
   document.querySelector("#temperature-output").innerHTML = temp
 }
 getWData()
+//learnwebcode.github.io/bootcamp-pet-data/pets.json
+async function getPetData() {
+  const petPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+  const petData = await petPromise.json()
+  //console.log(petData)
+  //document.querySelector("#pet-output").innerHTML = petData[0].name
+  petData.forEach(element => {
+    console.log(element.species)
+  })
+}
+getPetData()
